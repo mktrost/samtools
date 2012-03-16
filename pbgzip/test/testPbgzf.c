673ecbe7830d0cebfbe9f7de37af3c49c73a2a02
mktrost <mktrost@umich.edu>
2012-03-16 11:14:52 -0400

    Add more tests

--- pbgzip/test/testPbgzf.c
+++ pbgzip/test/testPbgzf.c
@@ -38,5 +38,49 @@ int main(int argc, char ** argv)
     // Read again after at the end of the file.
     assert(pbgzf_read(pbgzfHandle, testBuffer, 2) == 0);
 
+    pbgzf_close(pbgzfHandle);
+
+     ////////////////////////////
+    // Test again.
+    pbgzfHandle = pbgzf_open("data/bamFile.bam", "rb");
+
+    assert(pbgzf_tell(pbgzfHandle) == 0);
+    assert(pbgzf_read(pbgzfHandle, testBuffer, 4) == 4);
+    assert(pbgzf_read(pbgzfHandle, testBuffer, 2) == 2);
+    // 37 - 4 - 2 = 37 - 6 = 31 left to read
+    assert(pbgzf_read(pbgzfHandle, testBuffer, BUFFER_SIZE) == 31);
+
+    // Read again after at the end of the file.
+    assert(pbgzf_read(pbgzfHandle, testBuffer, 2) == 0);
+
+    // Rewind the file.
+    assert(pbgzf_seek(pbgzfHandle, 0, SEEK_SET) == 0);
+    assert(pbgzf_tell(pbgzfHandle) == 0);
+    assert(pbgzf_read(pbgzfHandle, testBuffer, 4) == 4);
+
+    assert(pbgzf_read(pbgzfHandle, testBuffer, BUFFER_SIZE) == 33);
+    // Read again after at the end of the file.
+    assert(pbgzf_read(pbgzfHandle, testBuffer, 2) == 0);
+
+    pbgzf_close(pbgzfHandle);
+
+   
+    ////////////////////////////
+    // Test again.
+    pbgzfHandle = pbgzf_open("data/bamFile.bam", "rb");
+
+    assert(pbgzf_tell(pbgzfHandle) == 0);
+    assert(pbgzf_read(pbgzfHandle, testBuffer, BUFFER_SIZE) == TEST_FILE_SIZE);
+    pbgzf_tell(pbgzfHandle);
+    assert(pbgzf_read(pbgzfHandle, testBuffer, BUFFER_SIZE) == 0);
+    assert(pbgzf_read(pbgzfHandle, testBuffer, BUFFER_SIZE) == 0);
+    // Rewind the file.
+    assert(pbgzf_seek(pbgzfHandle, 0, SEEK_SET) == 0);
+    assert(pbgzf_tell(pbgzfHandle) == 0);
+    assert(pbgzf_read(pbgzfHandle, testBuffer, 4) == 4);
+
+    pbgzf_close(pbgzfHandle);
+
+
     return(0);
 }
